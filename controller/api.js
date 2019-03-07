f209586c51f28b6c95e05f8c0b993acdd9ccc75d
immozart <vadimpostoffice@mail.ru>
2019-03-07 20:23:25 +0300

    начал писать поиск артистов по id

--- controller/api.js
+++ controller/api.js
@@ -1,8 +1,8 @@
 const fetch = require("node-fetch");
 const queryString = require("querystring");
 
-const getUrl = (artist) => {
-  const base_url = 'https://api.discogs.com/database/search?'
+const getIdUrl = (artist) => {
+  const base_url = 'https://api.discogs.com/database/search?';
   const params = {
     q: artist,
     key: 'dxgYyWTxQlEXIhnSSKSC',
@@ -13,20 +13,33 @@ const getUrl = (artist) => {
   return url;
 }
 
+const getArtistUrl = (id) => {
+  const base_url = 'https://api.discogs.com/{artist_id}/releases';
+
+};
+
+const getAlbums = async (artist) => {
+
+
+};
+
 const getArtistId = async (artist) => {
-  const url = getUrl(artist);
+  const url = getIdUrl(artist);
   const regex = new RegExp(`^${artist.toLowerCase()}.*`);
   const res = await fetch(url);
   const json = await res.json();
   let artists = [];
   for (let item of json.results) {
     if (item.title.toLowerCase().match(regex) && item.type === 'artist') {
-      artists.push(item.title);
+      let artObj = {};
+      artObj.title = item.title;
+      artObj.id = item.id;
+      artists.push(artObj);
     }
   }
+  
 }
 
-const a = getArtistId('The Jez');
-console.log(a)
+getArtistId('The Jez');
 
 module.exports = getUrl;
